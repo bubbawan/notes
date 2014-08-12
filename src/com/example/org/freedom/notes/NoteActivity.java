3993c9892e81dadef015feeb0b1f93bda4d60702
bubbawan <devyiduff@gmail.com>
2014-08-12 09:57:32 +0200

    NoteActivity added

---
+++ src/com/example/org/freedom/notes/NoteActivity.java
@@ -0,0 +1,81 @@
+package com.example.org.freedom.notes;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NoteActivity extends Activity {
+
+	private static String INTENT_ACTION_KEY = "action";
+	private static String INTENT_ACTION_CREATE = "create";
+	private static String INTENT_ACTION_EDIT = "edit";
+	private static String INTENT_ACTION_EDIT_ID = "edit-id";
+
+	public static Intent INTENT_CREATE(final Context context) {
+		return createBasicIntent(context).putExtra(INTENT_ACTION_KEY,
+				INTENT_ACTION_CREATE);
+	}
+
+	public static Intent INTENT_EDIT(final Context context,
+			final String id) {
+		return createBasicIntent(context).putExtra(INTENT_ACTION_KEY,
+				INTENT_ACTION_EDIT).putExtra(INTENT_ACTION_EDIT_ID, id);
+	}
+
+	private static Intent createBasicIntent(final Context context) {
+		return new Intent(context, NoteActivity.class);
+	}
+
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_note);
+		handleIntent();
+	}
+
+	private void handleIntent() {
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras == null) {
+			showError();
+			return;
+		}
+
+		String action = extras.getString(INTENT_ACTION_KEY);
+		if (action == null) {
+			showError();
+			return;
+		}
+		if (INTENT_ACTION_CREATE.equals(action)) {
+
+		} else if (INTENT_ACTION_EDIT.equals(action)) {
+
+		}
+	}
+
+	private void showError() {
+		System.out.println("NoteActivity.showError()");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.note, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- src/com/example/org/freedom/notes/StartActivity.java
+++ src/com/example/org/freedom/notes/StartActivity.java
@@ -4,24 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
 
+	private Button createButton;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+		setTitle("Notes");
+
+		createButton = (Button) findViewById(R.id.action_create_new);
+		createButton.setOnClickListener(new CreateHandler());
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	protected void onResume() {
+		super.onResume();
+		createButton.setEnabled(true);
+	}
+
+	private class CreateHandler implements OnClickListener {
+
+		@Override
+		public void onClick(final View v) {
+			createButton.setEnabled(false);
+			startActivity(NoteActivity.INTENT_CREATE(StartActivity.this));
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.start, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(final MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
