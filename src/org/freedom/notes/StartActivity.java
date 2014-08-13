596b1fcf6a8da25805cf78010ef13606e834f2a5
bubbawan <devyiduff@gmail.com>
2014-08-14 00:06:18 +0200

    ActionBar styling

--- src/org/freedom/notes/StartActivity.java
+++ src/org/freedom/notes/StartActivity.java
@@ -9,28 +9,21 @@ import org.freedom.notes.model.Note;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ListView;
 
 public class StartActivity extends NotesBasicActivity {
 
 	@InjectView(id = R.id.list_notes)
 	private ListView notesList;
-	@InjectView(id = R.id.action_create_new)
-	private Button createButton;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		createButton.setOnClickListener(new CreateHandler());
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		createButton.setEnabled(true);
 		bindList();
 	}
 
@@ -48,16 +41,6 @@ public class StartActivity extends NotesBasicActivity {
 		@Override
 		protected void deleteNote(final Note note) {
 
-		}
-
-	}
-
-	private class CreateHandler implements OnClickListener {
-
-		@Override
-		public void onClick(final View v) {
-			createButton.setEnabled(false);
-			startActivity(NoteActivity.INTENT_CREATE(StartActivity.this));
 		}
 
 	}
@@ -81,8 +64,12 @@ public class StartActivity extends NotesBasicActivity {
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		switch (id) {
+		case R.id.action_create_new:
+			startActivity(NoteActivity.INTENT_CREATE(StartActivity.this));
 			return true;
+		default:
+			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
